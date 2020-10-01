dbc121616cae3f4e2d326b8356da822e5bce1996
Tyler-Gatech <tyler.gatech@gmail.com>
2020-10-01 14:08:55 -0600

    updated website with gif

--- r_code/debate_script/debate_script.R
+++ r_code/debate_script/debate_script.R
@@ -4,6 +4,15 @@ library(stringr)
 library(lubridate)
 library(tidyverse)
 library(ggplot2)
+library(gganimate)
+
+install.packages("gifski")
+library(gifski)
+
+install.packages("extrafont")
+library(extrafont)
+
+fonts()
 
 script_url <- "https://www.rev.com/blog/transcripts/donald-trump-joe-biden-1st-presidential-debate-transcript-2020"
 
@@ -21,7 +30,7 @@ sec_pattern <-  "[0-9]{2}$" #Find the last two digits
 
 
 #Chris Wallace time
-cw_time <- script%>% 
+cw_time <- html%>% 
   html_text() %>% 
   str_extract_all(cw) %>% 
   unlist() %>% 
@@ -40,7 +49,7 @@ cw_time <- script%>%
   #use View() to find the error
 
 #Donald Trump Time
-dt_time <- script%>% 
+dt_time <- html%>% 
   html_text() %>% 
   str_extract_all(dt) %>% 
   unlist() %>% 
@@ -59,7 +68,7 @@ dt_time <- script%>%
   #you could readjust hours and minutes, but we are just calculating the real time, so it has no effect for our purposes
   
 #Joe Biden Time
-jb_time <- script%>% 
+jb_time <- html%>% 
   html_text() %>% 
   str_extract_all(jb) %>% 
   unlist() %>% 
@@ -88,6 +97,10 @@ all_time <-
 
 #eventually, we should clean up that if the next speaker is the same person, then eliminate the rows
 
+#Adding time stamp
+all_time <- all_time %>% 
+  mutate(time_stamp = ymd_hms("2020-09-29 09:00:00", tz = "EST") + dseconds(real_time), 
+         time_stamp_5 = ymd_hms("2020-09-29 09:00:00", tz = "EST") + dseconds(floor(real_time/300)*300))
 
 #Let's plot a few simple visuals
 
@@ -128,72 +141,107 @@ all_time %>%
   ggtitle("Average Speaking Length (in seconds)") +
   theme(legend.position = "none")
   
-#create a grid of all possible times
-time_grid <- rep(seq(1,max(all_time$real_time)),3)
-speaker_grid <- c(rep("cw",max(all_time$real_time)),
-                  rep("dt",max(all_time$real_time)),
-                  rep("jb",max(all_time$real_time)))
-
-full_grid <- tibble(
-  time = time_grid, 
-  speaker = speaker_grid
-)
-
-#plot number of TURNS spoken throughout the evening
-#calc the turns by second throughout the evening
-speak_times <- all_time %>% 
-  group_by(speaker, real_time) %>% 
-  summarize(count = n()) 
-  
-full_grid2 <- full_grid %>% 
-left_join(speak_times, by = c("speaker" = "speaker", "time" = "real_time")) %>% 
-  arrange(time, speaker) %>% 
-  mutate(count = replace_na(count,0)) %>% 
-  group_by(speaker) %>% 
-  mutate(count_running = cumsum(count)) #drag the speaking counts
-
-#plot by second as line 
-full_grid2 %>% 
-  ggplot(aes(x = time, y = count_running, group = speaker, color = speaker)) +
-  geom_line() +
-  scale_color_manual(values = c("grey", "red", "blue"),
-                     labels = c("Chris Wallace", "Donald Trump", "Joe Biden")) +
-  theme_bw() +
-  ggtitle("Total Speaking Turns")+
-  xlab("Actual Debate Time (in seconds)")
-
-
-#same as above, except with seconds
-#plot number of SECONDS spoken throughout the evening
-#calc the turns by second throughout the evening
-speak_length<- all_time %>% 
+#speak turns
+speak_turns <- all_time %>% 
   filter(diff_next >= 0) %>% 
-  group_by(speaker, real_time) %>% 
-  summarize(speak_time = sum(diff_next)) 
+  group_by(speaker, real_time, time_stamp_5) %>% 
+  summarize(speak_turns = n()) %>% 
+  rowwise() %>% 
+  mutate(char_time = str_extract(time_stamp_5, "[0-9]{2}:[0-9]{2}")) %>% 
+  ungroup()
 
-full_grid3 <- full_grid %>% 
-  left_join(speak_length, by = c("speaker" = "speaker", "time" = "real_time")) %>% 
-  arrange(time, speaker) %>% 
-  mutate(speak_time = replace_na(speak_time,0)) %>% 
+View(  speak_turns %>%
+         group_by(speaker) %>% 
+         mutate(speak_turns_running = cumsum(speak_turns)))
+
+
+#plot total amount of turns speaking thorughout the evening
+p_turns <- 
+  speak_turns %>%
   group_by(speaker) %>% 
-  mutate(speak_time_running = cumsum(speak_time)) #drag the speaking counts
-
-#plot by second as line 
-full_grid3 %>% 
-  ggplot(aes(x = time, y = speak_time_running, group = speaker, color = speaker)) +
-  geom_line() +
+  mutate(speak_turns_running = cumsum(speak_turns)) %>%  #drag the speaking counts
+  ggplot() +
+  geom_line(aes(x = real_time, y = speak_turns_running, color = speaker), size =1) +
+  geom_text(x = 1500, y = 535, label = paste0(speak_turns$char_time, " EST"), 
+            check_overlap = T, size = 10, family = "mono") +
+#annotate(geom ="text",x = 500, y = 1000, label = ifelse(speak_turns$speaker == "dt", speak_turns$char_time,"")) +
+#  geom_label(aes("8:00")) +
   scale_color_manual(values = c("grey", "red", "blue"),
-                     labels = c("Chris Wallace", "Donald Trump", "Joe Biden")) +
-  theme_bw() +
-  ggtitle("Total Speaking Length (in seconds)") +
-  xlab("Actual Debate Time (in seconds)")
+                     labels = c("Moderator", "Donald Trump", "Joe Biden")) +
+    theme(
+      panel.background = element_rect(fill = "#eaeaea"),
+      panel.border = element_blank(),
+      panel.grid.major.y = element_line(size = 0.1, color = "black"),
+      panel.grid.minor = element_blank(),
+      plot.background = element_rect(fill = "#eaeaea"),
+      plot.title = element_text(size = 20, family = "mono", hjust = 0.5),
+      legend.title=element_blank(),
+      legend.background = element_rect(fill = "#eaeaea"),
+      legend.position = "bottom",
+      axis.title.x = element_text(family = "mono"),
+      axis.title.y = element_text(color="#993333", size=14, face="bold"),
+      axis.ticks = element_blank()
+    ) + 
+    #annotate("text", x = 1500, y = 535, label = paste0("Actual Time: 10:50", time_stamp_5), family =("mono")) +
+    ggtitle("Presidential Debate: Sep 29 2020 \n # of Turns Speaking") +
+    ylab("") +
+    xlab("") +
+    scale_x_discrete(breaks=c(0,2000, 4000, 6000))
+
+animate(p_turns + 
+          transition_reveal(real_time), fps = 20)
 
 
+setwd("./Desktop/websites/stock_blog/img/debate1_2020")
+anim_save("turns_animated.gif")
 
 #plot total amount of speaking thorughout the evening
+speak_length <- all_time %>% 
+  filter(diff_next >= 0) %>% 
+  group_by(speaker, real_time,time_stamp_5) %>% 
+  summarize(speak_time = sum(diff_next)) %>% 
+  rowwise() %>% 
+  mutate(char_time = str_extract(time_stamp_5, "[0-9]{2}:[0-9]{2}")) %>% 
+  ungroup()
 
 
+p_time <- 
+speak_length %>%
+  group_by(speaker) %>% 
+  mutate(speak_time_running = cumsum(speak_time)) %>%  #drag the speaking counts
+  ggplot() +
+  geom_line(aes(x = real_time, y = speak_time_running, color = speaker), size =1) +
+  #geom_text(x = 1500, y = 1800, label = paste0(speak_turns$char_time, " EST"), 
+   #         check_overlap = T, size = 10, family = "mono") +
+  #annotate(geom ="text",x = 500, y = 1000, label = ifelse(speak_turns$speaker == "dt", speak_turns$char_time,"")) +
+  #  geom_label(aes("8:00")) +
+  scale_color_manual(values = c("grey", "red", "blue"),
+                     labels = c("Moderator", "Donald Trump", "Joe Biden")) +
+  theme(
+    panel.background = element_rect(fill = "#eaeaea"),
+    panel.border = element_blank(),
+    panel.grid.major.y = element_line(size = 0.1, color = "black"),
+    panel.grid.minor = element_blank(),
+    plot.background = element_rect(fill = "#eaeaea"),
+    plot.title = element_text(size = 20, family = "mono", hjust = 0.5),
+    legend.title=element_blank(),
+    legend.background = element_rect(fill = "#eaeaea"),
+    legend.position = "bottom",
+    axis.title.x = element_text(family = "mono"),
+    axis.title.y = element_text(color="#993333", size=14, face="bold"),
+    axis.ticks = element_blank()
+  ) + 
+  #annotate("text", x = 1500, y = 535, label = paste0("Actual Time: 10:50", time_stamp_5), family =("mono")) +
+  ggtitle("Total Time Speaking") +
+  ylab("") +
+  xlab("") +
+  scale_x_discrete(breaks=c(0,2000, 4000, 6000))
 
+
+animate(p_time + 
+          transition_reveal(real_time), fps = 20)
+
+anim_save("time_animated.gif")
 
 
 
